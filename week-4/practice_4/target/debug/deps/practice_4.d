04729a765da2e4946a3da14a6300cddbde45b14a
Akashi King <maxaudu08@gmail.com>
2025-10-22 17:31:43 +0100

    (Class Pracitce)

---
+++ week-4/practice_4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\maxau\Documents\M.AuduCSC101\week-4\practice_4\target\debug\deps\practice_4.d: src\main.rs
+
+C:\Users\maxau\Documents\M.AuduCSC101\week-4\practice_4\target\debug\deps\practice_4.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice_5/target/debug/deps/practice_5.d
@@ -0,0 +1,5 @@
+C:\Users\maxau\Documents\M.AuduCSC101\week-4\practice_5\target\debug\deps\practice_5.d: src\main.rs
+
+C:\Users\maxau\Documents\M.AuduCSC101\week-4\practice_5\target\debug\deps\practice_5.exe: src\main.rs
+
+src\main.rs:
